0fb52727fce0287bb26849819e58687993acbdb2
Ken Ling <ken88ling@gmail.com>
2017-03-09 20:30:55 +0800

    update Get method and POST method

--- EmployeeService/Controllers/EmployeesController.cs
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -18,14 +18,53 @@ namespace EmployeeService.Controllers
             }
         }
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBEntities entities = new EmployeeDBEntities())
             {
-                return entities.Employees.FirstOrDefault(x => x.ID == id);
+                var entity = entities.Employees.FirstOrDefault(x => x.ID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id = " + id.ToString() + " not found");
+                }
             }
         }
 
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception exception)
+            {
+               return Request.CreateErrorResponse(HttpStatusCode.BadRequest, exception);
+            }
+           
+        }
+
+        public void Delete(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var employeeDelete = entities.Employees.FirstOrDefault(x => x.ID == id);
+                entities.Employees.Remove(employeeDelete);
+                entities.SaveChanges();
+            }
+        }
 
     }
 }
